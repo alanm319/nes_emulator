963b705938a19bed3abe65925cd3055037d729b7
Alan <alanm319@gmail.com>
2024-10-12 21:32:31 -0700

    run updates

--- src/cpu.rs
+++ src/cpu.rs
@@ -583,62 +583,6 @@ impl CPU {
         }
     }
 
-    pub fn load_and_run(&mut self, program: Vec<u8>) {
-        self.load(program);
-        self.reset();
-        self.run()
-    }
-
-    pub fn reset(&mut self) {
-        self.register_a = 0;
-        self.register_x = 0;
-        self.register_y = 0;
-        self.status = StatusFlags::empty();
-        self.stack_pointer = STACK_RESET;
-        self.program_counter = self.mem_read_u16(0xFFFC);
-    }
-
-    pub fn load(&mut self, program: Vec<u8>) {
-        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
-        self.mem_write_u16(0xFFFC, 0x8000);
-    }
-
-    pub fn run(&mut self) {
-        //Hashmap where keys are actual opcodes and values are opcodes objects
-        let ref opcodes_map: HashMap<u8, &'static opcodes::OpCode> = *opcodes::OPCODES_MAP;
-
-        loop {
-            let code = self.mem_read(self.program_counter);
-            self.program_counter += 1;
-            let opcode = opcodes_map.get(&code).expect(&format!("OpCode {:x} is not recognized", code));
-            let prev_pc = self.program_counter;
-            match code {
-                // LDA
-                0xa9 | 0xa5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1 => {
-                    self.lda(&opcode.mode);
-                }
-                
-                //STA
-                0x85 | 0x95 | 0x8d | 0x9d | 0x99 | 0x81 | 0x91 => {
-                    self.sta(&opcode.mode);
-                }
-
-                0xaa => self.tax(), // TAX
-
-                0x00 => return, // BRK
-
-                0xe8 => self.inx(), // INX
-
-                _ => todo!()
-            }
-            
-            if prev_pc == self.program_counter {
-                self.program_counter += (opcode.len - 1) as u16;
-            }
-            
-        }
-    }
-
     fn get_operand_address(&self, mode: &AddressingMode) -> u16 {
         match mode {
             AddressingMode::Immediate => self.program_counter,
@@ -690,6 +634,198 @@ impl CPU {
             AddressingMode::Indirect | AddressingMode::NoneAddressing => {
                 panic!("mode {:?} is not supported", mode);
             }
+        }
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run()
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.register_y = 0;
+        self.status = StatusFlags::empty();
+        self.stack_pointer = STACK_RESET;
+        self.program_counter = self.mem_read_u16(0xFFFC);
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.mem_write_u16(0xFFFC, 0x8000);
+    }
+
+    pub fn run(&mut self) {
+        //Hashmap where keys are actual opcodes and values are opcodes objects
+        let ref opcodes_map: HashMap<u8, &'static opcodes::OpCode> = *opcodes::OPCODES_MAP;
+
+        loop {
+            let code = self.mem_read(self.program_counter);
+            self.program_counter += 1;
+            let opcode = opcodes_map.get(&code).expect(&format!("OpCode {:x} is not recognized", code));
+            let prev_pc = self.program_counter;
+            match code {
+
+                0x69 | 0x65 | 0x75 | 0x6D | 0x7D | 0x79 | 0x61 | 0x71 => {
+                    self.adc(&opcode.mode);
+                }
+
+                0x29 | 0x25 | 0x35 | 0x2D | 0x3D | 0x39 | 0x21 | 0x31 => {
+                    self.and(&opcode.mode);
+                }
+
+                0x0A | 0x06 | 0x16 | 0x0E | 0x1E => {
+                    self.asl(&opcode.mode);
+                }
+
+                0x90 => self.bcc(),
+
+                0xB0 => self.bcs(),
+
+                0xF0 => self.beq(),
+
+                0x24 | 0x2C => self.bit(&opcode.mode),
+
+                0x30 => self.bmi(),
+
+                0xD0 => self.bne(),
+
+                0x10 => self.bpl(),
+
+                0x00 => return, // BRK
+
+                0x50 => self.bvc(),
+
+                0x70 => self.bvs(),
+
+                0x18 => self.clc(),
+
+                0xD8 => self.cld(),
+
+                0x58 => self.cli(),
+
+                0xB8 => self.clv(),
+
+                0xC9 | 0xC5 | 0xD5 | 0xCD | 0xDD | 0xD9 | 0xC1 | 0xD1 => {
+                    self.cmp(&opcode.mode);
+                }
+
+                0xE0 | 0xE4 | 0xEC => {
+                    self.cpx(&opcode.mode);
+                }
+
+                0xC0 | 0xC4 | 0xCC => {
+                    self.cpy(&opcode.mode);
+                }
+
+                0xC6 | 0xD6 | 0xCE | 0xDE => {
+                    self.dec(&opcode.mode);
+                }
+
+                0xCA => self.dex(),
+
+                0x88 => self.dey(),
+
+                0x49 | 0x45 | 0x55 | 0x4D | 0x5D | 0x59 | 0x41 | 0x51 => {
+                    self.eor(&opcode.mode);
+                }
+
+                0xE6 | 0xF6 | 0xEE | 0xFE => {
+                    self.inc(&opcode.mode);
+                }
+
+                0xE8 => self.inx(),
+
+                0xC8 => self.iny(),
+
+                0x4C | 0x6C => self.jmp(&opcode.mode),
+
+                0x20 => self.jsr(),
+                
+                0xA9 | 0xa5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1 => {
+                    self.lda(&opcode.mode);
+                }
+
+                0xA2 | 0xA6 | 0xB6 | 0xAE | 0xBE => {
+                    self.ldx(&opcode.mode);
+                }
+
+                0xA0 | 0xA4 | 0xB4 | 0xAC | 0xBC => {
+                    self.ldy(&opcode.mode);
+                }
+
+                0x4A | 0x46 | 0x56 | 0x4E | 0x5E => {
+                    self.lsr(&opcode.mode);
+                }
+
+                0xEA => {}
+
+                0x09 | 0x05 | 0x15 | 0x0D | 0x1D | 0x19 | 0x01 | 0x11 => {
+                    self.ora(&opcode.mode);
+                }
+
+                0x48 => self.pha(),
+
+                0x08 => self.php(),
+
+                0x68 => self.pla(),
+
+                0x28 => self.plp(),
+
+                0x2A | 0x26 | 0x36 | 0x2E | 0x3E => {
+                    self.rol(&opcode.mode);
+                }
+
+                0x6A | 0x66 | 0x76 | 0x6E | 0x7E => {
+                    self.ror(&opcode.mode);
+                }
+
+                0x40 => self.rti(),
+
+                0x60 => self.rts(),
+
+                0xE9 | 0xE5 | 0xF5 | 0xED | 0xFD | 0xF9 | 0xE1 | 0xF1 => {
+                    self.sbc(&opcode.mode);
+                }
+
+                0x38 => self.sec(),
+
+                0xF8 => self.sed(),
+
+                0x78 => self.sei(),
+
+                0x85 | 0x95 | 0x8D | 0x9D | 0x99 | 0x81 | 0x91 => {
+                    self.sta(&opcode.mode);
+                }
+
+                0x86 | 0x96 | 0x8E => {
+                    self.stx(&opcode.mode);
+                }
+
+                0x84 | 0x94 | 0x86 => {
+                    self.sty(&opcode.mode);
+                }
+
+                0xAA => self.tax(),
+
+                0xA8 => self.tay(),
+
+                0xBA => self.tsx(),
+
+                0x8A => self.txa(),
+
+                0x9A => self.txs(),
+
+                0x98 => self.tya(),
+
+                unknown_code => panic!("{:?} is an unknown opcode", unknown_code)
+            }
+            
+            if prev_pc == self.program_counter {
+                self.program_counter += (opcode.len - 1) as u16;
+            } 
         }
     }
 }
@@ -746,5 +882,4 @@ mod test {
         assert_eq!(cpu.register_a, 0x55);
     }
 
-    
 }
